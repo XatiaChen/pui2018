a50c5334a4efa73118e594fb08f079f791200ec4
XatiaChen <xchen386@wisc.edu>
2018-10-01 17:22:32 -0400

    Add files via upload

---
+++ week_5lab/main.js
@@ -0,0 +1,36 @@
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+
+
+<button onclick="addNewList()">Add New List</button>
+
+
+function addListItem() {
+    console.log('hello world');
+}
