97a0d7e16f36112a042b259b177330781df225ee
Xiangzhu Chen <cxzxatia@outlook.com>
2018-11-30 21:23:41 -0500

    duedate1

--- homework8/assets/scripts/mainix.js
+++ homework8/assets/scripts/mainix.js
@@ -1,46 +1,153 @@
 $(document).ready(function() {
-    //classic plays scroll effect
 
-    // var ti = 0;
-    // var ScrollInstance = ScrollReveal();
-    // var currentPlay = 0;
-    // var timer = 0;
-    //
-    // function scrollElement(){
-    //     ScrollInstance.reveal("#sub1-content2", {
-    //         delay: 200,
-    //         duration: 300,
-    //         origin:"top",
-    //         distance:"500px",
-    //         opacity:1,
-    //     });
-    // }
-    // function transformPlayList(currentPlay) {
-    //     $(".sub-content").css("transform", `translateY(-${currentPlay * 100}vh)`);
-    //     $("#nav-sub1").css("top", `${40 - currentPlay * 28}px`);
-    //     $("#nav-sub1")
-    //         .find("li")
-    //         .removeClass("active")
-    //         .eq(currentPlay)
-    //         .addClass("active");
-    // }
-    // $("#sub1-content1").on("wheel", function(e) {
-    //     e.stopPropagation();
-    //     var deltaY = e.originalEvent.deltaY;
-    //     clearTimeout(timer);
-    //     timer = setTimeout(function() {
-    //         if (deltaY > 0) {
-    //             if (currentPlay < 2) {currentPlay += 1;}
-    //         }
-    //         else {
-    //             if (currentPlay < 0) {currentPlay -= 1;}
-    //
-    //         }
-    //     });
+    $('.opt').on('click', function(event) {
+        var t = event.target;
+        var id = t.id;
+        var children = t.dataset.children;
+        $('.active-content').removeClass('active-content').addClass('deactive-content');
+        if (children) {
+            $('#' + id).siblings('.active-opt').removeClass('active-opt').addClass('deactive-opt');
+            $('#' + id).removeClass('deactive-opt').addClass('active-opt');
+            $('#' + children).removeClass('deactive-sub').addClass('active-sub');
+            //$('#' + children).children().first().removeClass('deactive-sub-opt').addClass('active-sub-opt');
+            //$('#' + $('#' + children).children().first()[0].dataset.content).removeClass('deactive-content').addClass('active-content');
+        } else {
+            $('.nav-sub').removeClass('active-sub').addClass('deactive-sub');
+            //$('.nav-sub').children('active-sub-opt').removeClass('active-sub-opt').addClass('deactive-sub-opt');
+            $('#' + id).siblings('.active-opt').removeClass('active-opt').addClass('deactive-opt');
+            $('#' + id).removeClass('deactive-opt').addClass('active-opt');
+        }
+        var content = t.dataset.content;
+        $('#' + content).removeClass('deactive-content').addClass('active-content');
+    });
+
+    // $('.sub-opt').on('click', function(event) {
+    //     var id = event.target.id;
+    //     $('#' + id).siblings('.active-sub-opt').removeClass('active-sub-opt').addClass('deactive-sub-opt');
+    //     $('#' + id).removeClass('deactive-sub-opt').addClass('active-sub-opt');
     // });
+
+    $('.sub-opt').on('click', function(event) {
+        var id = event.target.id;
+        var content = event.target.dataset.content;
+        var upperMarginTop = 7;
+        var idx = $('.nav-bar .nav-sub').children().index(this)
+        var currentMarginTop = upperMarginTop - 2 * idx;
+        $('.nav-bar .nav-sub').css('margin-top', currentMarginTop + 'rem');
+        $('#' + content).siblings('.active-content').fadeOut().removeClass('active-content').addClass('deactive-content');
+        $('#' + content).fadeIn().removeClass('deactive-content').addClass('active-content');
+        $('#' + id).siblings('.active-sub-opt').removeClass('active-sub-opt').addClass('deactive-sub-opt');
+        $('#' + id).removeClass('deactive-sub-opt').addClass('active-sub-opt');
+    });
+
+
+    // music play
+    var music = musicController();
+
+    $("#audio1").on("click",function(){
+        music.play("audio1");
+        //show intro
+        $("#zhuqiang-intro").css("visibility", "visible");
+        $("#huaqiang-intro").css("visibility", "hidden");
+        $("#xianqiang-intro").css("visibility", "hidden");
+
+        //show wave
+        $("#wave1").css("visibility", "visible");
+        $("#wave2").css("visibility", "hidden");
+        $("#wave3").css("visibility", "hidden");
+    })
+    $("#audio2").on("click",function(){
+        music.play("audio2");
+        //show intro
+        $("#zhuqiang-intro").css("visibility", "hidden");
+        $("#huaqiang-intro").css("visibility", "visible");
+        $("#xianqiang-intro").css("visibility", "hidden");
+
+        //show wave
+        $("#wave1").css("visibility", "hidden");
+        $("#wave2").css("visibility", "visible");
+        $("#wave3").css("visibility", "hidden");
+    })
+    $("#audio3").on("click",function(){
+        music.play("audio3");
+        //show intro
+        $("#zhuqiang-intro").css("visibility", "hidden");
+        $("#huaqiang-intro").css("visibility", "hidden");
+        $("#xianqiang-intro").css("visibility", "visible");
+
+        //show wave
+        $("#wave1").css("visibility", "hidden");
+        $("#wave2").css("visibility", "hidden");
+        $("#wave3").css("visibility", "visible");
+    })
+
+    $("#yan-musicsample").on("click",function(){
+        music.play("audio4");
+        // $("#yan-musicsample").css("background-image", "url(&quot;../images/yanmusicpause.png&quot;)")
+    })
+
     //
+    function  musicController() {
+        //Stop the music when click the button again
+        function stop(audio) {
+            audio.currentTime = 0;
+            audio.pause();
+        }
+
+        var audio1 = document.createElement("audio");
+        audio1.src="./assets/music/operaclip_zhuqiang.mp3";
+
+        var audio2 = document.createElement("audio");
+        audio2.src="./assets/music/operaclip_huaqiang.mp3";
+
+        var audio3 = document.createElement("audio");
+        audio3.src="./assets/music/operaclip_xianqiang.mp3";
+
+        var audio4 = document.createElement("audio");
+        audio4.src="./assets/music/yanfengyingxuanduan.mp3";
+
+        return {
+            play:function(audio) {
+                switch (audio) {
+                    case "audio1":{
+                        audio1.paused?audio1.play():stop(audio1);
+                        audio2.pause();
+                        audio3.pause();
+                        audio4.pause();
+                        break;
+                    }
+                    case "audio2":{
+                        audio1.pause();
+                        audio2.paused?audio2.play():stop(audio2);
+                        audio3.pause();
+                        audio4.pause();
+                        break;
+                    }
+
+                    case "audio3":{
+                        audio1.pause();
+                        audio2.pause();
+                        audio3.paused?audio3.play():stop(audio3);
+                        audio4.pause();
+                        break;
+                    }
+
+                    case "audio4":{
+                        audio1.pause();
+                        audio2.pause();
+                        audio3.pause();
+                        audio4.paused?audio4.play():stop(audio4)
+                        break;
+                    }
 
 
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
 
     //costume flip pop up window
     $("#costume-detail").on("click", function() {
@@ -55,14 +162,4 @@ $(document).ready(function() {
             .find(".popup-content")
             .removeClass("animated flipInY");
     });
-
-    function transformPlayList(currentPlay) {
-        $("#play-list").css("transform", `translateY(-${currentPlay * 100}vh)`);
-        $("#plays-menu").css("top", `${40 - currentPlay * 28}px`);
-        $("#plays-menu")
-            .find("li")
-            .removeClass("active")
-            .eq(currentPlay)
-            .addClass("active");
-    };
 });
